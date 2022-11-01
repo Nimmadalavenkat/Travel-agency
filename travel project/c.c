56906ac2ca5a2a396034f495c02f36dfa510e805
Nimmadalavenkat <117196336+Nimmadalavenkat@users.noreply.github.com>
2022-11-01 21:40:33 +0530

    Add files via upload

---
+++ travel project/c.c
@@ -0,0 +1,53 @@
+#include <stdbool.h>
+#include <stdio.h>
+ 
+
+bool checkDuplicatesWithinK(int arr[], int n, int k)
+{
+
+    // traversing the input array
+
+    for (int i = 0; i < n; i++) {
+
+        int j = i + 1;
+
+        int range = k;
+
+        // searching in next k-1 elements if its duplicate
+
+        // is present or not
+
+        while (range > 0 && j < n) {
+
+            if (arr[i] == arr[j])
+
+                return true;
+
+            j++;
+
+            range--;
+
+        }
+
+    }
+
+    return false;
+}
+ 
+// Driver method to test above method
+
+int main()
+{
+
+    int arr[] = { 10, 5, 3, 4, 3, 5, 6 };
+
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    if (checkDuplicatesWithinK(arr, n, 3))
+
+        printf("Yes");
+
+    else
+
+        printf("No");
+}
